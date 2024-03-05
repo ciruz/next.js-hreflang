bdc15fc2386c1cdd8047a578e8636aaff9d94392
Thomas Dietrich <dietrich.tom@gmail.com>
2024-03-05 22:38:04 +0100

    add code from example + ts type-check

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
+import type { Metadata } from "next";
+
 /** Add your relevant code here for the issue to reproduce */
+export const metadata: Metadata = {
+  title: "test",
+  alternates: {
+    canonical: "https://example.com", hreflang: { "en-US": "https://example.com/en-US" }
+  }
+}
 export default function Home() {
   return null;
 }
